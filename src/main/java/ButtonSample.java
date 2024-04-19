4a24955fd8fc477e3580f0bf4d2a2f4ad5fe3cce
sophie.phan <sophiephan364@gmail.com>
2024-04-19 15:45:28 +0100

    Add mouse listener

--- src/main/java/ButtonSample.java
+++ src/main/java/ButtonSample.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ButtonSample {
     public static void main(String[] args) {
@@ -10,7 +13,39 @@ public class ButtonSample {
 
             AnActionListener actionListener = new AnActionListener();
 
+            MouseListener mouseListener = new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent mouseEvent) {
+                    if (SwingUtilities.isLeftMouseButton(mouseEvent)) {
+                        System.out.println("Left button pressed.");
+                    }
+                    if (SwingUtilities.isRightMouseButton(mouseEvent)) {
+                        System.out.println("Right button pressed.");
+                    }
+                    if (SwingUtilities.isMiddleMouseButton(mouseEvent)) {
+                        System.out.println("Middle button pressed.");
+                    }
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent mouseEvent) {
+                    if (SwingUtilities.isLeftMouseButton(mouseEvent)) {
+                        System.out.println("Left button released.");
+                    }
+                    if (SwingUtilities.isRightMouseButton(mouseEvent)) {
+                        System.out.println("Right button released.");
+                    }
+                    if (SwingUtilities.isMiddleMouseButton(mouseEvent)) {
+                        System.out.println("Middle button released.");
+                    }
+                    System.out.println();
+                }
+
+            };
+
+
             button.addActionListener(actionListener);
+            button.addMouseListener(mouseListener);
 
             frame.add(button, BorderLayout.SOUTH);
             frame.setSize(300, 100);
